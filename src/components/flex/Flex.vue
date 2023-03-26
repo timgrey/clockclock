97230fd26f1b70fa1375684e9e2cbf37cc6616b4
Tim grey <timothy.a.grey@gmail.com>
2023-03-26 17:37:19 -0500

    comments for flex component

--- src/components/flex/Flex.vue
+++ src/components/flex/Flex.vue
@@ -20,14 +20,49 @@
 
 <script setup lang="ts">
 defineProps({
+  /**
+   * Applies styles "flex-direction: row-reverse;
+   */
   rowReverse: Boolean,
+
+  /**
+   * Applies styles "flex-direction: column;"
+   */
   column: Boolean,
+
+  /**
+   * Applies styles "flex-direction: column-reverse;"
+   */
   columnReverse: Boolean,
+
+  /**
+   * Applies styles "justify-content: center;"
+   */
   justifyCenter: Boolean,
+
+  /**
+   * Applies styles "justify-content: space-around;"
+   */
   justifySpaceAround: Boolean,
+
+  /**
+   * Applies styles "justify-content: space-between;"
+   */
   justifySpaceBetween: Boolean,
+
+  /**
+   * Applies styles "justify-content: flex-end;"
+   */
   justifyFlexEnd: Boolean,
+
+  /**
+   * Applies styles "align-items: center;"
+   */
   alignCenter: Boolean,
+
+  /**
+   * Applies styles "align-items: flex-end;"
+   */
   alignFlexEnd: Boolean,
 
   /**
